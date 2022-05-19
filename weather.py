05dc534c66e9823e0c905525bf8b2c365376adad
Treveon <88203620+daisuket330@users.noreply.github.com>
2022-05-19 14:30:54 -0400

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,8 +1,14 @@
-from urllib import request
-import requests
-
-api_key ="9122543327dbd0a9cb68a52feda751ac"
+api_key = "9122543327dbd0a9cb68a52feda751ac"
 city = "Tampa"
-url ="https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
+flat = 27.964157
+flon = -82.452606
+# url = "http://api.openweathermap.org/data/2.5/weather?lat={lat}&{lon}&"&appid="+api_key
 
-request = requests.get
+# https://api.openweathermap.org/data/2.5/weather?lat=27&lon=-82&appid=9122543327dbd0a9cb68a52feda751ac
+
+url = "https://api.openweathermap.org/data/2.5/weather?lat=27&lon=-82&appid=9122543327dbd0a9cb68a52feda751ac"
+
+request = requests.get(url)
+
+json = request.json
+print(json)
